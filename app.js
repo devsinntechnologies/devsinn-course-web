1438476dfbad0fc2c895e1ad85cfff4d7afd893e
AbuzarZaidi <devsinntechnologies@gmail.com>
2024-09-24 02:42:11 +0500

    Static serve website with express js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "website" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
