55b4947f5f5140b216a92b90ae5c047b8216aefc
AbuzarZaidi <devsinntechnologies@gmail.com>
2024-09-24 02:44:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve static files from the "website" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
